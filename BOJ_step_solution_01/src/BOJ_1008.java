daf708ccbe5e162628afec86f7f013593471b7f2
kkw-knu <rlarjsdn529@naver.com>
2021-04-02 01:46:29 +0900

    백준 단계별문제풀이 1

---
+++ BOJ_step_solution_01/src/BOJ_1008.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class BOJ_1008 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a / b);
+	}
+}
